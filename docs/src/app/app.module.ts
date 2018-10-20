0b9ab6b737bdbb7729e0a2ee0556bd73c602d3ef
CoteVD <majovardon@gmail.com>
2018-10-20 17:48:47 -0300

    Deploy

---
+++ docs/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDNOu2JQ001PxZY-GVwFvVou0_6h_Sj-14'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ docs/src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  public stores: String;
+
+  title: String;
+
+  constructor() {}
+
+  ngOnInit() {
+    fetch('../../assets/JSON_data/store_directory.json')
+    .then(res => res.json())
+    .then(stores => {
+      console.log(stores);
+      this.stores = stores;
+    });
+  }
+
+}
